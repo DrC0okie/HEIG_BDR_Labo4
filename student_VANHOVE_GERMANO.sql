9497e64343cfd4b4ccc68ed63bde68b6f386e45e
Thomas Germano <thomas.germano@heig-vd.ch>
2022-11-29 15:07:36 +0100

    exo 2 and 4

--- student_VANHOVE_GERMANO.sql
+++ student_VANHOVE_GERMANO.sql
@@ -5,9 +5,48 @@ SET search_path = pagila;
 ---------------------------
 
 -- Exercice 01
+
 -- END Exercice 01
 
 -- Exercice 02
+CREATE TABLE staff_creation_log
+(
+    username     varchar(16) NOT NULL,
+    when_created timestamp NOT NULL
+);
+
+CREATE OR REPLACE FUNCTION staff_creation_log()
+    RETURNS TRIGGER
+    AS
+$$
+BEGIN
+    INSERT INTO staff_creation_log VALUES(NEW.username, now());
+    RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER staff_creation
+    BEFORE INSERT
+    ON staff
+    FOR EACH ROW
+    EXECUTE PROCEDURE staff_creation_log();
+
+INSERT INTO staff (first_name, last_name, address_id, email, store_id, username, password)
+            values
+                ('Thomas',
+                 'Germano',
+                 50,
+                 'thomas.germano@sekilastaff.com',
+                 1,
+                 'catwayne',
+                 'wallah'
+                );
+
+SELECT username, when_created
+FROM staff_creation_log
+WHERE username = 'catwayne';
+
 -- END Exercice 02
 
 -- Exercice 03
@@ -18,12 +57,24 @@ SET search_path = pagila;
 ---------------------------
 
 -- Exercice 04
+CREATE OR REPLACE VIEW staff_address
+AS
+SELECT phone, first_name, last_name, address, postal_code, city, district, country
+    FROM staff
+    INNER JOIN address a
+        ON a.address_id = staff.address_id
+    INNER JOIN city c
+        ON c.city_id = a.city_id
+    INNER JOIN country co
+        ON c.country_id = co.country_id
+;
 -- END Exercice 04
 
 -- Exercice 05
 -- END Exercice 05
 
 -- Exercice 06
+
 -- END Exercice 06
 
 -- Exercice 07
